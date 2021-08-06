b18ea8b9c077ca893bd28bc196a8d5a9ec34025c
Juan Pablo Chica <juanpchica@hotmail.com>
2021-08-06 11:44:28 -0500

    Creating components and folders for project structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import './App.scss';
 import { Navbar } from './components/Navbar/Navbar';
+
+//React router
+import { BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import { Home } from './pages/Home';
+import { Product } from './pages/Product';
+import { Service } from './pages/Service';
+import { Footer } from './components/Footer/Footer';
+
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar/>
-    </div>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/product" component={Product}/>
+        <Route path="/service" component={Service}/>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
 
