846a402d9a49da92c5901570793bf5b6a4b0f030
Juan Pablo Chica <juanpchica@hotmail.com>
2021-08-06 12:00:34 -0500

    Navbar structure added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import "./Navbar.scss";
 
+//Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome,faStreetView,faProcedures,faBalanceScaleLeft,faTimes,faBars } from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = () => {
+
+    const [menuOpen,setMenuOpen] = useState(false)
+
     return (
-        <nav className="navbar">
-            <div className="navbar-container">
-                nav
+        <header>
+            <div className="container header-container flex">
+                <div className="brand-icon">
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faBalanceScaleLeft} /> BRAND
+                    </Link>
+                </div>
+                <nav>
+                    <div className="navbar-icon">
+                        {menuOpen ?<FontAwesomeIcon icon={faTimes} /> : <FontAwesomeIcon icon={faBars} />}
+                    </div>
+                    <div className="navbar-container">
+                        <ul>
+                            <li>
+                                <Link to="/">
+                                    <FontAwesomeIcon icon={faHome} /> Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    <FontAwesomeIcon icon={faProcedures} /> Service
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    <FontAwesomeIcon icon={faStreetView} /> Product
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
-        </nav>
+        </header>
+        
     ) 
 }
